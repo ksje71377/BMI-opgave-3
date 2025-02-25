1b37c2f7ccd63a31253f54741423b2929a502d73
ksje713777 <ksje71377@edu.ucl.dk>
2025-02-25 11:50:49 +0100

    Add project files.

---
+++ BMI/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BMI
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double weight;
+            double height;
+            double bmiFinish;
+
+            Console.WriteLine("BMI tool");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Tast vægt");
+            weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tast højde");
+            height = Convert.ToDouble(Console.ReadLine());
+            bmiFinish = weight / Math.Pow(height, 2);
+            Console.WriteLine("\n\n\n\tDin BMI er: {0}", bmiFinish);
+            Console.ReadKey();
+
+        }
+    }
+}
